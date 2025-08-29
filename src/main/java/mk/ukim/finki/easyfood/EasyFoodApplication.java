09b63e81ec991605e64a859b841de94e18103044
Ece Micaiti <ecemucaiti2004@gmail.com>
2025-08-29 16:58:24 +0200

    initial commit

---
+++ src/main/java/mk/ukim/finki/easyfood/EasyFoodApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.easyfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyFoodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyFoodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mk/ukim/finki/easyfood/EasyFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.easyfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyFoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
